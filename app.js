5a51ac543c5b6b4dd0f1b85335daba16bfcf0e47
juan.cibils <juan.cibils@devgurus.io>
2021-10-12 12:37:41 -0300

    feat(app) add a query and a mutation

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+
+      type RootMutation {
+        createEvent(name: String): String
+      }
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Hello", "World"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
+
+const PORT = 3001;
+app.listen(PORT);
+console.log("Graphql-React-Booking-Events Server listen on port", PORT);
